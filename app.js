aa5c2fab78c1fc0e019bd97ca179832b0df493e6
vagrant <vagrant@vagrant.vm>
2017-01-19 07:27:03 +0000

    Updated chats. TODO: DB storage of chats

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var socketio = require('socket.io'); 
+var socketio = require('socket.io');
 var express = require('express');
-var bodyParser  = require('body-parser');
-var morgan      = require('morgan');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
 var path = require('path');
 
-var jwt    = require('jsonwebtoken'); 
-var config = require('./config'); 
+var jwt = require('jsonwebtoken');
+var config = require('./config');
 var mongoose = require('mongoose');
 
 mongoose.connect(config.database, function(err) {
-	if (err) throw err;
+    if (err) throw err;
 });
-var User   = require('./models/user'); 
+var User = require('./models/user');
 
 var port = process.env.PORT || 3000;
 var websocket_port = 3001
@@ -20,97 +20,100 @@ app.set('superSecret', config.secret);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
-app.use('/public/js',  express.static(__dirname + '/public/js'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use('/public/js', express.static(__dirname + '/public/js'));
 //var apiRoutes = express.Router(); 
 app.get('/', function(req, res) {
     res.send('Hello! The API is at http://localhost:' + port + '/api');
 });
 //app.use('/',  express.static(__dirname + ''));
 app.get('/login', function(req, res) {
-    	res.sendFile( path.join(__dirname, '/public/login.html'));
+    res.sendFile(path.join(__dirname, '/public/login.html'));
 });
 
 app.get('/chat', function(req, res) {
-    	res.sendFile( path.join(__dirname, '/public/chat.html'));
+    res.sendFile(path.join(__dirname, '/public/chat.html'));
 });
 
 app.get('/signup', function(req, res) {
-    	res.sendFile(path.join(__dirname + '/public/register.html'));
+    res.sendFile(path.join(__dirname + '/public/register.html'));
 });
 
 app.post('/api/authenticate', function(req, res) {
-	User.getByUsername(req.body.username).then(function(user) {
-    		if (!user) {
-      			res.json({ success: false, message: 'Authentication failed. User was not found.' });
-    		} else if (user) {
-		 	if (user.password != req.body.password) {
-        				res.json({ success: false, message: 'Authentication failed. Invalid credentials.' });
-      		  	} else {
-        				var token = jwt.sign(user, app.get('superSecret'), {});
-        				console.log("[*] Auth:", user.username)
-        				res.json({
-          					success: true,
-          					message: 'Enjoy your token!',
-          					token: token,
-          					username: user.username
-        				});
-      			}   
+    User.getByUsername(req.body.username).then(function(user) {
+        if (!user) {
+            res.json({ success: false, message: 'Authentication failed. User was not found.' });
+        } else if (user) {
+            if (user.password != req.body.password) {
+                res.json({ success: false, message: 'Authentication failed. Invalid credentials.' });
+            } else {
+                var token = jwt.sign(user, app.get('superSecret'), {});
+                console.log("[*] Auth:", user.username)
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token,
+                    username: user.username
+                });
+            }
 
-    		}
+        }
 
-  	}).catch(function(error){
-		res.json({ success: false, message: 'Authentication failed. ' + error.message });
-  	});		
+    }).catch(function(error) {
+        res.json({ success: false, message: 'Authentication failed. ' + error.message });
+    });
 });
 
 app.post('/api/users', function(req, res) {
-	var name = req.body.name;
-	var username = req.body.username;
-	var password = req.body.password;
-	var confirm = req.body.confirm;
-	if (!name || !username || !password || !confirm) {
-		res.status(400)
-		res.json({ success: false, message: 'Invalid Parameter.' });	
-	}
-	if (password != confirm) {
-		res.status(400)
-		res.json({ success: false, message: 'Passwords do no match.' });	
-	}
-	User.createOrUpdate({
-		name: name,
-		username: username,
-		password: password
-	}).then(function(user) {
-		res.status(201)
-		res.json(user);	
-	}).catch(function(err) {
-		res.status(500)
-		res.json({success: false, message: err.message});	
-	});
+    var name = req.body.name;
+    var username = req.body.username;
+    var password = req.body.password;
+    var confirm = req.body.confirm;
+    if (!name || !username || !password || !confirm) {
+        res.status(400)
+        res.json({ success: false, message: 'Invalid Parameter.' });
+    }
+    if (password != confirm) {
+        res.status(400)
+        res.json({ success: false, message: 'Passwords do no match.' });
+    }
+    User.createOrUpdate({
+        name: name,
+        username: username,
+        password: password
+    }).then(function(user) {
+        res.status(201)
+        res.json(user);
+    }).catch(function(err) {
+        res.status(500)
+        res.json({ success: false, message: err.message });
+    });
 });
 
 var sockets = {};
 
 
-socketio.listen(websocket_port).on('connection', function (socket) { 
+socketio.listen(websocket_port).on('connection', function(socket) {
     console.log('Connection established');
-    socket.on('message', function (msg) { 
-    	if (msg && msg.conection) {
-    		sockets[msg.connection] = socket;
-	        	socket.broadcast.emit('connection', msg.connection); 
-   		return;
-    	}
-    	if (msg.message && msg.user) {
-    		sockets[msg.user].send(msg.message);
-    	}
-        	console.log('Message Received: ', msg); 
-    }); 
+    setInterval(function() {
+        socket.send({ 'connection': Object.keys(sockets) });
+    }, 10000)
+    socket.on('message', function(msg) {
+        if (msg && msg.connection) {
+            console.log('broadcasting');
+            sockets[msg.connection] = socket;
+            socket.broadcast.emit({ 'connection': Object.keys(sockets) });
+            socket.send({ 'connection': Object.keys(sockets) });
+            return;
+        }
+        if (msg.message && msg.to && msg.from) {
+            console.log('SEnding messagej')
+            sockets[msg.to].send(msg);
+        }
+        console.log('Message Received: ', msg);
+    });
 });
 
-app.listen(port, function(){
+app.listen(port, function() {
     console.log('Express server listening on port ' + port);
 });
-
-
-
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,89 +1,95 @@
-
-
-var app = angular.module("testapp",[
-   'ngStorage'
+var app = angular.module("testapp", [
+    'ngStorage'
 ]);
 
 app.factory("DataModel", function() {
-  var Service = {};
-  
-  
-  
-  return Service;
+    var Service = {};
+
+
+
+    return Service;
 });
 
-app.config(function($locationProvider){
-    $locationProvider.html5Mode({enabled: true, requireBase: false}).hashPrefix('!');
+app.config(function($locationProvider) {
+    $locationProvider.html5Mode({ enabled: true, requireBase: false }).hashPrefix('!');
 });
 
 app.controller("ChatController", function($scope, $location, $localStorage) {
     $scope.users = [];
+    $scope.chatMessagesAllUser = {};
     var token = localStorage.getItem('access_token');
     var username = localStorage.getItem('user');
-  var iosocket = io.connect('http://localhost:3001'); 
-      iosocket.on('connect', function () { 
-      $('#incomingChatMessages').append($('<p>Connected</p>')); 
-    iosocket.send({connection: username});
-    iosocket.on('message', function(message) { 
-        if(message.connection) {
-            console.log(message.connection);
-            if ($scope.users.indexOf(message.connection) == -1) {
-                $scope.users.push(message.connection);
+    var iosocket = io.connect('http://localhost:3001');
+    iosocket.on('connect', function() {
+        $('#incomingChatMessages').append($('<p>Connected</p>'));
+        iosocket.send({ connection: username });
+        iosocket.on('message', function(message) {
+            if (message.from && message.to && message.message) {
+                console.log(message);
+                console.log("TO ME", username, message.to);
+                var chat = $scope.formatChat("http://placehold.it/16x16", message.from, message.message, new Date());
+                $scope.chatMessagesAllUser[message.from].push(message.message);
+                $scope.chatMessages.push(chat);
                 $scope.$apply();
+                return;
             }
-            return;
-        }
-        console.log(message);
-        //$('#incomingChatMessages').append($('<li></li>').text(message)); 
-    }); 
-    iosocket.on('disconnect', function() { 
-    $('#incomingChatMessages').append('<p>Disconnected</p>'); 
-  }); 
-  });
+            if (message.connection) {
+                console.log(message.connection);
+                $scope.users = message.connection.filter(function(item) {
+                    return item != username;
+                });
+                $scope.$apply();
+                return;
+            }
+            //$('#incomingChatMessages').append($('<li></li>').text(message)); 
+        });
+        iosocket.on('disconnect', function() {
+            $('#incomingChatMessages').append('<p>Disconnected</p>');
+        });
+    });
 
 
-  $scope.chatMessagesAllUser = {};
-  $scope.chatToUser = function(user) {
+
+    $scope.chatToUser = function(user) {
         console.log('[*] Chatting with ', user);
-        if (!$scope.chatMessagesAllUser[user.username]) {
-            $scope.chatMessagesAllUser[user.username] = [];
+        if (!$scope.chatMessagesAllUser[user]) {
+            $scope.chatMessagesAllUser[user] = [];
         }
-        $scope.chatMessages = $scope.chatMessagesAllUser[user.username];
-        $scope.selectedUser = user.username;
-  }
+        $scope.chatMessages = $scope.chatMessagesAllUser[user];
+        $scope.selectedUser = user;
+    }
 
-  $scope.chatMessages = [];
-  $scope.selectedUser = null;
-  
-  $scope.formatChat = function(icon,username,text,origDt) {
-    var chat = {};
-    chat.icon = icon;
-    chat.username = username;
-    chat.text = text;
-    chat.origDt = origDt;
-    return chat;
-  }
-  
-  $scope.addChat = function(username) {
-    if ($scope.newChatMsg != "") {
-      var chat = $scope.formatChat("http://placehold.it/16x16",
-                           username,
-                           $scope.newChatMsg,
-                           new Date());
-       
-      $scope.chatMessages.push(chat);
-      io.send({user: username, message: $scope.newChatMsg});
-      $scope.newChatMsg = "";
+    $scope.chatMessages = [];
+    $scope.selectedUser = null;
+
+    $scope.formatChat = function(icon, username, text, origDt) {
+        var chat = {};
+        chat.icon = icon;
+        chat.username = username;
+        chat.text = text;
+        chat.origDt = origDt;
+        return chat;
     }
-    else {
-      
+
+    $scope.addChat = function(_username) {
+        if ($scope.newChatMsg != "") {
+            var chat = $scope.formatChat("http://placehold.it/16x16",
+                username,
+                $scope.newChatMsg,
+                new Date());
+
+            $scope.chatMessages.push(chat);
+            iosocket.send({ from: username, to: _username, message: $scope.newChatMsg });
+            $scope.newChatMsg = "";
+        } else {
+
+        }
     }
-  }
-  
+
 });
 
 app.filter('reverse', function() {
-  return function(items) {
-    return items.slice().reverse();
-  };
+    return function(items) {
+        return items.slice().reverse();
+    };
 });
